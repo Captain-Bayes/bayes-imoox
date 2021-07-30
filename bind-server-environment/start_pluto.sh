d783e4c30311692a6b714a5f585749cf97e2a2e7
michi7x7 <michael@sieberer.me>
2021-07-30 10:34:33 +0200

    stupid mistakkkkkk

--- bind-server-environment/start_pluto.sh
+++ bind-server-environment/start_pluto.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env bash
 
 cd notebooks
-julia -e "import PlutoSliderServer; PlutoSliderServer.run_directory('.'; SliderServer_port=1234, SliderServer_host='0.0.0.0')"
+julia -e 'import PlutoSliderServer; PlutoSliderServer.run_directory("."; SliderServer_port=1234, SliderServer_host="0.0.0.0")'
